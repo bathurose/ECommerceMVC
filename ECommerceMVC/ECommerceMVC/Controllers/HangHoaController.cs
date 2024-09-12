36563cbf69201543eb2efec574cc984240687d09
bathurose <dt.24698@gmail.com>
2024-09-12 22:14:20 +0700

    f commit

---
+++ ECommerceMVC/ECommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly Hshop2023Context db;
+
+        public HangHoaController(Hshop2023Context context)  
+        {
+            db = context;
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai);
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                Hinh = p.Hinh ?? "",
+                DonGia = p.DonGia?? 0,
+                MoTaNgan = p.MoTaDonVi?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+    
+            return View(result);
+        }
+    }
+}
---
+++ ECommerceMVC/ECommerceMVC/ViewModels/HangHoaVM.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+
+        public string? TenHh { get; set; }
+
+        public string? Hinh { get; set; }
+
+        public double DonGia { get; set; }
+
+        public string? MoTaNgan {  get; set; }
+
+        public string? TenLoai {  get; set; }
+
+    }
+}
